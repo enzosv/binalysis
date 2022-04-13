8778fa56eaf0f0c232416829d90cd4ccc6d38b4e
Enzo Vergara <vergara.enzo@gmail.com>
2022-04-13 22:06:09 +0800

    gzip fileserver

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/Kucoin/kucoin-go-sdk"
+	"github.com/NYTimes/gziphandler"
 	binance2 "github.com/adshao/go-binance/v2"
 	common "github.com/adshao/go-binance/v2/common"
 	"github.com/binance-exchange/go-binance"
@@ -122,8 +123,8 @@ func main() {
 	r.HandleFunc("/latest", LatestHandler(*store, *verbose)).Methods("GET")
 	r.HandleFunc("/update", UpdateHandler(*store, *verbose)).Methods("POST")
 	r.HandleFunc("/del", DeleteHandler(*store, *verbose)).Methods("DELETE")
-	// r.PathPrefix("/").Handler(gziphandler.GzipHandler(http.FileServer(http.Dir("./web/"))))
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./web/")))
+	r.PathPrefix("/").Handler(gziphandler.GzipHandler(http.FileServer(http.Dir("./web/"))))
+	// r.PathPrefix("/").Handler(http.FileServer(http.Dir("./web/")))
 	if *verbose {
 		fmt.Printf("running at %d\nstoring at %s\n", *port, *store)
 	}
